da3cd0e08b2e7be2c6a2dda7e159b88654fea013
Yuttapong Dongngam <yuttapong@hotmail.com>
2021-10-05 14:12:01 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app-grid">
+        <div className="box-left">
+          <img className="imgg" src="/images/yutt.jpg" />
+
+          <div className="name">
+            <h3>YUTTAPONG DONGNGAM</h3>
+            
+          </div>
+
+          <div className="front">
+            <h3>FRONT-END DEVELOPER</h3>
+          </div>
+
+          <div className="pro">
+            <h3>PROFILE</h3>
+          </div>
+
+          <p  className="text-color">
+            If i have the opportunity. I would like to do front-end jop and I
+            will continue to develop myself. And won't stop learning abot React.
+          </p>
+
+          <div className="skills">
+            <h3>SKILLS</h3>
+          </div>
+      
+          <p  className="text-color">-JavaScript (ES6)</p>
+          <p>-HTML, CSS</p>
+          <p>-React.js</p>
+          <p>-Git</p>
+          <p> -Vs code</p>
+          
+
+          <div className="con">
+            <h3>CONTACT DETAILS</h3>
+          </div>
+
+          <p>Telephone: 0981610462</p>
+          <p>Email:Yuttapong001@hotmail.com</p>
+          <p>Bangkok</p>
+        </div>
+
+        <div>
+          <div className="exp">
+            <h4>EXPERIENCE</h4>
+          </div>
+
+          <h4>ASIA HOTEL (2019-2021)</h4>
+          <p>-Has the duty of cooking and cost-effective food</p>
+
+          <h4>TOT PUBLIC COMPANY LIMITED (2018-2019) (Internship)</h4>
+
+          <p>-TOT Debt Collection Center (2 Months)</p>
+          <p>
+            -Issuing an area to educate about the Pracharat Internet (2 Months)
+          </p>
+
+          <h4>PROJECT IN UNIVERSITY</h4>
+          <p>-Windows Applications Parking system with Visual Studio Program</p>
+
+          <div className="edu">
+            <h4>EDUCATION</h4>
+          </div>
+
+          <h4>Rajabhat Mahasarakham University</h4>
+          <p>-Management Science ( Computer Business )</p>
+          <h4>Pathumrat Pittayakom School ( Roi-et )</h4>
+
+          <div className="lan">
+            <h4>LANGUAGES</h4>
+          </div>
+
+          <p>-Thai</p>
+          <p>-English (middle level)</p>
+
+          <div className="hob">
+            <h4>HOBBIES & INTERESTS</h4>
+          </div>
+
+          <p>-Play Guitar</p>
+          <p>-Play Sports</p>
+          <p>-play Game</p>
+          <p>-Read Book</p>
+          <p>-Social</p>
+        </div>
+      </div>
     </div>
   );
 }
